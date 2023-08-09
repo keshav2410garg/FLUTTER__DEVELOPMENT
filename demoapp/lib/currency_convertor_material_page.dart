6858bf3436381fa034962c763adacebfb5c210af
keshav2410garg <55792010+keshav2410garg@users.noreply.github.com>
2023-08-10 00:16:41 +0530

    Build the Complete UI

--- demoapp/lib/currency_convertor_material_page.dart
+++ demoapp/lib/currency_convertor_material_page.dart
@@ -5,26 +5,56 @@ class CurrencyCovertorMaterialPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext content) {
-    return const Scaffold(
+    double result = 0;
+    final TextEditingController texteditingcotroller = TextEditingController();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black12,
+        title: const Text(
+          'CURRENCY CONVERTOR',
+          style: TextStyle(color: Colors.black12),
+        ),
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('CUNRRENCY CONVERTOR',
-                style: TextStyle(
+             Text(result.toString(),
+                style: const TextStyle(
                   fontSize: 35,
                   fontWeight: FontWeight.bold,
                 )),
-            TextField(
-              style: TextStyle(
-                color: Colors.black87,
-              ),
-              decoration: InputDecoration(
-                hintText: 'Please Enter the amount in INR',
-                hintStyle: TextStyle(
-                    color: Colors.black87, fontWeight: FontWeight.bold),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                controller: texteditingcotroller,
+                style: const TextStyle(
+                  color: Colors.black87,
+                ),
+                decoration: const InputDecoration(
+                  hintText: 'Please Enter the amount in INR',
+                  hintStyle: TextStyle(
+                      color: Colors.black87, fontWeight: FontWeight.bold),
+                  prefixIcon: Icon(Icons.monetization_on_outlined),
+                  prefixIconColor: Colors.brown,
+                  filled: true,
+                  fillColor: Colors.white,
+                  focusedBorder: OutlineInputBorder(),
+                ),
+                keyboardType: TextInputType.number,
               ),
             ),
+            ElevatedButton(
+                onPressed: () {
+                  result=double.parse(texteditingcotroller.text * 83));
+                },
+                style: const ButtonStyle(
+                  elevation: MaterialStatePropertyAll(15),
+                  backgroundColor: MaterialStatePropertyAll(Colors.black),
+                  foregroundColor: MaterialStatePropertyAll(Colors.amber),
+                  fixedSize: MaterialStatePropertyAll(Size(200, 50)),
+                ),
+                child: const Text('CONVERT'))
           ],
         ),
       ),
